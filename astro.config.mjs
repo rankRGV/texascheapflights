bf7326a91d11b9cda4335ac5710fd2adc3fcb8e8
DontGatoSideDev <154033472+Temp-Dev-69@users.noreply.github.com>
2026-03-03 19:53:38 -0600

    fix: disable astro checkOrigin security setting to allow resend webhooks via POST

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  security: {
+    checkOrigin: false
+  },
   adapter: vercel({
     webAnalytics: {
       enabled: false,
